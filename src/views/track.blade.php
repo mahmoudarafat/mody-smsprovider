fb19274c71b51d98d386915eaad2677f0e2ec3aa
Mahmoud Arafat <mahmoudarafat33@gmail.com>
2018-08-22 18:02:53 +0200

    Add files via upload

---
+++ src/views/track.blade.php
@@ -0,0 +1,57 @@
+@extends('smsprovider::layouts.master')
+
+@section('content')
+
+    <div class="container" style="margin-top:3em;">
+        <div class="row">
+
+            <div id="message-info"></div>
+            <div class="col-md-12">
+                <h1 class="pager text-primary">{{ trans('smsprovider::smsgateway.track_group_title') }}</h1>
+                <hr>
+            </div>
+            @if($track->count() > 0)
+                <div class="col-md-10 col-md-offset-1">
+
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                        <tr class="info">
+                            <th>#</th>
+                            <th>{{ trans('smsprovider::smsgateway.date') }}</th>
+                            <th>{{ trans('smsprovider::smsgateway.details') }}</th>
+                        </tr>
+                        </thead>
+                        <tbody id="table-body">
+                        @foreach($track->chunk(20) as $item_ch)
+                            @foreach($item_ch as $item)
+                                <tr id="itemrow-{{ $item->id }}">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $item->created_at }}</td>
+                                    <td class="text-info">{{ $item->description }}</td>
+
+                                </tr>
+                            @endforeach
+                        @endforeach
+
+                        </tbody>
+                    </table>
+
+                    <div class="text-center">
+                        {!! $track->links() !!}
+                    </div>
+                </div>
+            @else
+                <h3 class="text-center text-muted">{{ trans('smsprovider::smsgateway.empty_info') }}</h3>
+            @endif
+        </div>
+    </div>
+
+@stop
+
+@section('styles')
+
+@stop
+
+@section('scripts')
+
+@stop
